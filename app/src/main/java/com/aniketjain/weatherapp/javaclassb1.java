539d36693d415521599b7a7c3ef3c361489999c9
minhazabrar <minhazabrar05@gmail.com>
2023-03-23 04:03:47 +0600

    java file added in branch1

---
+++ app/src/main/java/com/aniketjain/weatherapp/javaclassb1.java
@@ -0,0 +1,27 @@
+package com.aniketjain.weatherapp;
+
+public class javaclassb1 {
+    Integer roll;
+    String name;
+
+    public javaclassb1(Integer roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
