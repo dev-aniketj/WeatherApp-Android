0a8b4ef05728553bcd3790de48a6413dd0ec4215
minhazabrar <minhazabrar05@gmail.com>
2023-03-23 03:59:46 +0600

    java file added

--- app/src/main/java/com/aniketjain/weatherapp/abrar04.java
+++ app/src/main/java/com/aniketjain/weatherapp/abrar04.java
@@ -1,2 +1,27 @@
-package com.aniketjain.weatherapp;public class abrar04 {
+package com.aniketjain.weatherapp;
+
+public class abrar04 {
+    Integer roll;
+    String name;
+
+    public abrar04(Integer roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public Integer getRoll() {
+        return roll;
+    }
+
+    public void setRoll(Integer roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
